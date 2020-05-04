ae92c685a8ab1782c74af4201355431e9afafb07
SEYEONi <seyeon9704@gmail.com>
2020-05-04 19:43:33 +0900

    PS

---
+++ 2020123456.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define n1_max 15000000
+#define n2_max 150000000
+
+int n1[n1_max];
+int n2[n2_max];
+
+int input[3];
+int n1_num;
+int n2_num;
+
+int answer = 0; //final common
+
+
+// Find common part of two array and reserve at global variable 'answer' You can change function to int function if you want to return value.
+void find_common(){
+
+
+
+
+
+}
+
+
+int main()
+{
+
+FILE *fp =  fopen("sample1_answer.txt", "r");
+FILE *fp2 = fopen("sample1_n2.txt", "r");
+FILE *fp3 = fopen("sample1_n1.txt", "r");
+
+
+for(int i =0; i<3; i++){
+        fscanf(fp, "%d", &input[i]);
+}
+
+fclose(fp);
+
+n2_num = input[0];
+n1_num = input[1];
+int common = input[2]; //Correct Answer!
+
+for(int i =0; i<n2_num; i++){
+        fscanf(fp2, "%d", &n2[i]);
+}
+
+for(int i =0; i<n1_num; i++){
+        fscanf(fp3, "%d", &n1[i]);
+}
+
+
+fclose(fp2);
+fclose(fp3);
+
+find_common();
+
+printf("Real_Common : %d\n",common ); //Real answer
+printf("Calculated_Common : %d\n",answer ); //Your guess
+
+
+return 0;
+
+}
